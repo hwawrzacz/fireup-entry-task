f0d69de68059b8b4deaabf081a8c1a1c24c57514
Hubert <hwawrzacz.dev@gmail.com>
2021-03-01 14:29:53 +0100

    Add home page
    Add new module, component and update routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,28 @@
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatCarouselModule } from '@ngbmodule/material-carousel';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { SliderComponent } from './components/slider/slider.component';
-import { MatCarouselModule } from '@ngbmodule/material-carousel';
-import { MatIconModule } from '@angular/material/icon';
+import { HomePageModule } from './pages/home-page/home-page/home-page.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    SliderComponent
+    SliderComponent,
   ],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     MatCarouselModule,
-    MatIconModule
+    MatIconModule,
+    HomePageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
