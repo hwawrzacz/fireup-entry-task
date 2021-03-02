031b387eea0903116127c7eef021fa5f4fdc4f82
Hubert <hwawrzacz.dev@gmail.com>
2021-03-02 20:06:17 +0100

    Add basic card properties

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  private _imageUrl: string = '';
+  private _imageAltText: string = '';
+  private _caption: string = '';
+  private _imageStyle: 'circle' | 'polygon' = 'circle';
+
+  @Input('imageUrl')
+  set imageUrl(value: string) {
+    this._imageUrl = value
+  }
+  get imageUrl(): string {
+    return this._imageUrl;
+  }
+
+  @Input('imageAltText')
+  set imageAltText(value: string) {
+    this._imageAltText = value;
+  }
+  get imageAltText(): string {
+    return this._imageAltText;
+  }
+
+  @Input('imageStyle')
+  set imageStyle(value: 'circle' | 'polygon') {
+    this._imageStyle = value;
+  }
+  get imageStyle(): 'circle' | 'polygon' {
+    return this._imageStyle;
+  }
+
+  @Input('caption')
+  set caption(value: string) {
+    this._caption = value;
+  }
+  get caption(): string {
+    return this._caption;
+  }
+
 
   constructor() { }
 
