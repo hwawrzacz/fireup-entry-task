8a0563c2bc37b74bd35ec0af057d892fd2b90af3
Hubert <hwawrzacz.dev@gmail.com>
2021-03-03 21:08:33 +0100

    Add list items and style them

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,11 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ListComponent implements OnInit {
+  private readonly _title = 'Title'
+  private readonly _items: string[] = [
+    'Nisi eu esse proident minim cillum. Aliqua ut pariatur do incididunt elit aute esse. Mollit est id dolore officia eu duis veniam.',
+    'Nisi eu esse proident minim cillum. Aliqua ut pariatur do incididunt elit aute esse. Mollit est id dolore officia eu duis veniam.',
+    'Nisi eu esse proident minim cillum. Aliqua ut pariatur do incididunt elit aute esse. Mollit est id dolore officia eu duis veniam.',
+    'Nisi eu esse proident minim cillum. Aliqua ut pariatur do incididunt elit aute esse. Mollit est id dolore officia eu duis veniam.',
+    'Nisi eu esse proident minim cillum. Aliqua ut pariatur do incididunt elit aute esse. Mollit est id dolore officia eu duis veniam.',
+  ]
+
+  get title(): string {
+    return this._title;
+  }
+
+  get items(): string[] {
+    return this._items;
+  }
 
   constructor() { }
 
