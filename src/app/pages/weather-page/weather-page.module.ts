d0bf42e2356c7603de96cd673b2fcb219b6815ad
Hubert <hwawrzacz.dev@gmail.com>
2021-03-04 11:43:40 +0100

    Add empty weather page component

---
+++ src/app/pages/weather-page/weather-page.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WeatherPageComponent } from './weather-page.component';
+
+@NgModule({
+  declarations: [WeatherPageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class WeatherPageModule { }
