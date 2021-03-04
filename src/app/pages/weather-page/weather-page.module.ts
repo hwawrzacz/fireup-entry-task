0343d1296ca836ac4d37c7e47e3ae1a81a9dcb4e
Hubert <hwawrzacz.dev@gmail.com>
2021-03-04 14:07:15 +0100

    Add weather page template

--- src/app/pages/weather-page/weather-page.module.ts
+++ src/app/pages/weather-page/weather-page.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WeatherPageComponent } from './weather-page.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [WeatherPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatProgressSpinnerModule,
   ]
 })
 export class WeatherPageModule { }
